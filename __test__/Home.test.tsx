3d956b86b6513a998e056233de39f0c531c580e7
furuta03 <hibiki19961208@gmail.com>
2021-10-27 15:54:03 +0900

    [add]srcディレクトリの追加

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Home from '../pages/index'
+import Home from '../src/pages/index'
 
 it('Should render title text', () => {
   render(<Home />)
