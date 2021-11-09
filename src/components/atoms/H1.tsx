c0f531b148aedd36a1cf66a9daf3c80862a19b64
furuta03 <hibiki19961208@gmail.com>
2021-11-09 16:47:12 +0900

    [update]H1コンポーネントの更新 スタイルの追加とクラス情報をpropsで渡す

--- src/components/atoms/H1.tsx
+++ src/components/atoms/H1.tsx
@@ -1,10 +1,15 @@
 import React, { ReactNode } from 'react'
 import { VFC } from 'react'
+import styles from '@/styles/components/atoms/H1.module.scss';
 
 interface PROPS {
-  children: ReactNode
+  children: ReactNode,
+  className?: string
 }
 
-const H1: VFC<PROPS> = ({ children }) => <h1>{ children }</h1>
+const H1: VFC<PROPS> = ({
+  children,
+  className 
+}) => <h1 className={`${ className } ${ styles.h1 }`}>{ children }</h1>
 
 export default H1
