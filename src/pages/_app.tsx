4a17c8661cdbd4cb986f0f9700aecb4bfb7fcc38
furuta03 <hibiki19961208@gmail.com>
2021-11-09 13:35:37 +0900

    [update]material-uiのtheme設定をグローバルに適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
+import '@/styles/globals.scss'
+import theme from '@/styles/theme'
 import { AppProps } from 'next/dist/shared/lib/router/router'
-import '../styles/globals.css'
 import { StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+      </ThemeProvider>
     </StyledEngineProvider>
   )
 }
