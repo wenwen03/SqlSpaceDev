84b3faf8ecc73063edce4281d1f57b5e36ba27ff
furuta03 <hibiki19961208@gmail.com>
2021-11-09 16:48:24 +0900

    [update]Pコンポーネントの更新 スタイル情報の追加とクラス情報をPropsで受け取る

--- src/components/atoms/P.tsx
+++ src/components/atoms/P.tsx
@@ -1,10 +1,15 @@
 import React, { ReactNode } from 'react'
 import { VFC } from 'react'
+import styles from '@/styles/components/atoms/P.module.scss'
 
 interface PROPS {
-  children: ReactNode
+  children: ReactNode,
+  className?: string
 }
 
-const P: VFC<PROPS> = ({ children }) => <p>{ children }</p>
+const P: VFC<PROPS> = ({
+  children,
+  className
+}) => <p className={`${className} ${styles.p}`}>{ children }</p>
 
 export default P
