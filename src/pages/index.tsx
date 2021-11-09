a2d7f5c67099eedf4a8c6f6228a18b40e85957af
furuta03 <hibiki19961208@gmail.com>
2021-11-09 16:54:59 +0900

    [update]indexページの更新 TopTextとImageを追加する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import  Layout  from '@/components/templates/Layout'
+import TopText from '@/components/organisms/TopText'
+import Image from 'next/image'
+import styles from '@/styles/pages/index.module.scss'
 
 export default function Home() {
   return (
-    <Layout title="hoge">
-      <h1>これがchildren</h1>
+    <Layout title="hoge"
+      wholeClass={ styles.index }
+      mainClass={ styles.main }
+      >
+      <TopText/>
+      <Image src='/img/top-img.svg' width={ 800 } height={ 500 }/>
     </Layout>
   )
 }
