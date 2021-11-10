de8795ebefd1a9c4ca068753e4456fdbb2df105a
furuta03 <hibiki19961208@gmail.com>
2021-11-10 12:04:58 +0900

    [update]Circleコンポーネントの更新 classNameをpropsで受け取る

--- src/components/atoms/Circle.tsx
+++ src/components/atoms/Circle.tsx
@@ -2,11 +2,15 @@ import { ReactNode, VFC } from "react"
 import styles from '@/styles/components/atoms/Circle.module.scss'
 
 interface PROPS {
-  children: ReactNode
+  children: ReactNode,
+  className: string
 }
 
-const Circle: VFC<PROPS> = ({ children }) => (
-  <div className={ styles.circle }>
+const Circle: VFC<PROPS> = ({
+    children,
+    className
+  }) => (
+  <div className={`${className} ${styles.circle}`}>
     { children }
   </div>
 )
