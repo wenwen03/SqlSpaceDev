8c112692200a2944489d21bc0f6e8b529efde238
furuta03 <hibiki19961208@gmail.com>
2021-11-10 16:41:06 +0900

    [update]Paperコンポーネント更新 elevation属性をセットできるようにする

--- src/components/atoms/Paper.tsx
+++ src/components/atoms/Paper.tsx
@@ -5,15 +5,18 @@ import styles from '@/styles/components/atoms/Paper.module.scss'
 interface PROPS {
   children: ReactNode,
   className?: string,
-  variant?: 'elevation' | 'outlined'
+  variant?: 'elevation' | 'outlined',
+  elevation?: number
 }
 const Paper: VFC<PROPS> = ({
   children,
   className,
-  variant = 'elevation'
+  variant = 'elevation',
+  elevation = 0
 }) => ( 
   <MUIPaper
     className={`${className} ${styles.paper}`}
+    elevation={ elevation}
     variant={variant}>
     { children }
   </MUIPaper>
