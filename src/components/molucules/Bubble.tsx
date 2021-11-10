da1cef6c30f2c2201a7851507630a9df0fd51e2a
furuta03 <hibiki19961208@gmail.com>
2021-11-10 16:42:09 +0900

    [add]Bubbleコンポーネントの実装

---
+++ src/components/molucules/Bubble.tsx
@@ -0,0 +1,26 @@
+import React, { VFC } from 'react'
+import Paper from '@/components/atoms/Paper'
+import styles from '@/styles/components/molucules/Bubble.module.scss'
+import Avatar from '@/components/atoms/ Avatar'
+import P from '../atoms/P'
+
+interface PROPS {
+  comment: string,
+  speaker: 'dog' | 'human'
+}
+
+const Bubble: VFC<PROPS> = ({
+  comment,
+  speaker
+}) => (
+  <Paper className={ styles.bubble } >
+    <Avatar
+      alt='charactor icon'
+      src={ speaker === 'dog'? '/img/dog-icon.jpg' : '/img/human-icon.png'}
+      className={ styles.avatar }
+    />
+    <P>{ comment}</P>
+  </Paper>
+)
+
+export default Bubble
