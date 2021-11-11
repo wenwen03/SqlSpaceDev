b1c0439ebdd060686f804ead64540d81203bf431
furuta03 <hibiki19961208@gmail.com>
2021-11-11 14:52:46 +0900

    [add]MissionCardコンポーネントの更新 classNameをpropsで受け取れるようにする

--- src/components/molucules/MissionCard.tsx
+++ src/components/molucules/MissionCard.tsx
@@ -1,22 +1,24 @@
 import Card from '@/components/atoms/Card'
 import H4 from '@/components/atoms/H4'
 import P from '@/components/atoms/P'
-import MissionPaper from '@/components/molucules/MIssionPaper'
+import MissionPaper from '@/components/molucules/MissionPaper'
 import React, { VFC } from 'react'
 import styles from '@/styles/components/molucules/MissionCard.module.scss'
 
 interface PROPS {
   title: string,
   description: string,
-  purposes: Array<string>
+  purposes: Array<string>,
+  className: string
 }
 
 const MissionCard: VFC<PROPS> = ({
   title,
   description,
-  purposes
+  purposes,
+  className
 }) => (
-  <Card>
+  <Card className={ className }>
     <H4>{ title }</H4>
     <P className={ styles.description }> { description }</P>
     <MissionPaper purposes={ purposes } />
