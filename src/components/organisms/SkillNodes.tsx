7fb92ccdfab031453ef455077361386ec56d437b
furuta03 <hibiki19961208@gmail.com>
2021-11-11 10:36:25 +0900

    [add]SkillNodesコンポーネントの実装

---
+++ src/components/organisms/SkillNodes.tsx
@@ -0,0 +1,64 @@
+import Grid from '@mui/material/Grid'
+import React, { VFC } from 'react'
+import SkillCircle from '../molucules/SkillCircle'
+import styles from '@/styles/components/organisms/SkillNodes.module.scss'
+
+const missions: Array<Mission> = [
+  { name: null, status: 'locked'},
+  { name: null, status: 'locked'},
+  { name: null, status: 'locked'},
+  { name: 'Insert', status: 'completed'},
+  { name: null, status: 'locked'},
+  { name: null, status: 'locked'},
+  { name: null, status: 'locked'},
+  { name: null, status: 'locked'},
+  { name: 'Select', status: 'available'},
+  { name: 'Update', status: 'available'},
+  { name: 'Delete', status: 'available'},
+  { name: 'Create', status: 'available'},
+  { name: null, status: 'locked'},
+  { name: 'Where', status: 'available'},
+  { name: 'Undefined Null', status: 'available'},
+  { name: null, status: 'locked'},
+  { name: 'Bulk Command', status: 'available'},
+  { name: null, status: 'locked'},
+  { name: 'Basic Function', status: 'available'},
+  { name: 'Ambiguous Search', status: 'available'},
+  { name: 'Range Search', status: 'available'},
+  { name: 'Group', status: 'available'},
+  { name: 'Arthmetic Operator', status: 'locked'},
+  { name: 'Index', status: 'locked'},
+  { name: 'Union Except Intersect', status: 'locked'},
+  { name: 'Pagenation', status: 'locked'},
+  { name: 'Aggregate Function', status: 'locked'},
+  { name: 'Join', status: 'locked'},
+  { name: 'Case', status: 'locked'},
+  { name: 'Performance', status: 'locked'},
+]
+
+interface Mission {
+  name?: string,
+  status: MissionStatus
+}
+
+type MissionStatus = 'completed' | 'available' | 'locked'
+
+const SkillNodes: VFC = () => (
+  <Grid
+    container
+    rowSpacing={1}
+    justifyContent='center'
+    alignItems='center'
+    className={ styles.skillTree }
+  >
+    { missions.map(( mission, index ) => (
+      <SkillCircle
+        skillName={mission.name}
+        status={mission.status}
+        key={index}
+      />
+    ))}
+  </Grid>
+)
+
+export default SkillNodes
