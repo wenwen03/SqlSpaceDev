8e25190d564651d9679cd6fb8209d316ca42e1fa
furuta03 <hibiki19961208@gmail.com>
2021-11-11 14:54:33 +0900

    [update]NameLabelコンポーネントの更新 classNameをpropsで受け取れるようにする

--- src/components/molucules/NameLabel.tsx
+++ src/components/molucules/NameLabel.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { VFC } from 'react'
 import H2 from '../atoms/H2'
 import LevelLabel from './LevelLabel'
 import styles from '@/styles/components/molucules/NameLabel.module.scss'
-import P from '../atoms/P'
 
-const NameLabel = () => (
-  <div className={styles.nameLabel}>
+interface PROPS {
+  className: string
+}
+
+const NameLabel: VFC<PROPS> = ({
+  className
+}) => (
+  <div className={`${ className } ${styles['name-label']}`}>
     <H2>Test User</H2>
     <LevelLabel level={1}/>
   </div>
