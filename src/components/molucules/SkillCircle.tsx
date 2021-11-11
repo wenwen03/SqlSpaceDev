e51b2f93e67f8d428b97c9b8c187b730ee318186
furuta03 <hibiki19961208@gmail.com>
2021-11-11 10:39:05 +0900

    [update]SkillCircleのスタイル更新 ステータスに応じてスタイルを変更させる

--- src/components/molucules/SkillCircle.tsx
+++ src/components/molucules/SkillCircle.tsx
@@ -5,17 +5,19 @@ import styles from '@/styles/components/molucules/SkillCircle.module.scss'
 
 interface PROPS {
   skillName?: string,
+  status?: 'locked' | 'available' | 'completed'
 }
 
 const SkillCircle: VFC<PROPS> = ({
   skillName,
+  status = 'locked'
 }) => {
   return skillName ? (
-    <Grid item xs={4}>
-      <Circle className={styles.skillCircle}>{ skillName }</Circle>
+    <Grid item xs={2} sx={{display: 'flex', justifyContent: 'center'}}>
+      <Circle className={styles[status]}>{ skillName }</Circle>
     </Grid>
   ) : (
-    <Grid item xs={4}>
+    <Grid item xs={2}>
     </Grid>
   )
 }
