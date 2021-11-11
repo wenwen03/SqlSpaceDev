7510f18e6bcb2b577d7767e5d2bed1472065261c
furuta03 <hibiki19961208@gmail.com>
2021-11-11 14:55:48 +0900

    [update]SkillTreeコンポーネント classNameをpropsで受け取れるようにする

--- src/components/organisms/SkillTree.tsx
+++ src/components/organisms/SkillTree.tsx
@@ -1,12 +1,19 @@
 import React, { VFC } from 'react'
 import SkillLines from './SkillLines'
 import SkillNodes from './SkillNodes'
+import styles from '@/styles/components/organisms/SkillTree.module.scss'
 
-const SkillTree: VFC = () => (
-  <>
-    <SkillNodes />
+interface PROPS {
+  className: string
+}
+
+const SkillTree: VFC<PROPS> = ({
+  className
+}) => (
+  <div className={`${ className } ${ styles['skill-tree']}`}>
+    <SkillNodes className={ styles['skill-node']}/>
     <SkillLines />
-  </>
+  </div>
 )
 
 export default SkillTree
