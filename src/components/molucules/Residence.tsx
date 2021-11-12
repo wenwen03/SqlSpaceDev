fce4ff22827cbbd131c1753f54a7e93eecf97b7b
furuta03 <hibiki19961208@gmail.com>
2021-11-12 17:04:40 +0900

    [update]Residenceコンポーネントの更新 onClickコールバック関数をPropsで受け取る

--- src/components/molucules/Residence.tsx
+++ src/components/molucules/Residence.tsx
@@ -2,10 +2,17 @@ import React, { VFC } from 'react'
 import MyImg from '@/components/atoms/MyImg'
 import styles from '@/styles/components/molucules/Architecture.module.scss'
 
-const Residence: VFC = () => (
+interface PROPS {
+  focusFunc: () => void
+}
+
+const Residence: VFC<PROPS> = ({
+  focusFunc
+}) => (
   <MyImg
     src='/img/architectures/residence.svg'
     alt='residence'
+    onClick={ focusFunc }
     className={ styles.architecture }
     width={ 300 }
     height={ 200 }
