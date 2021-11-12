50a5a282a193ff6a57552a739ad36e8b96ef6f52
furuta03 <hibiki19961208@gmail.com>
2021-11-12 17:06:00 +0900

    [update]WindowFrameコンポーネントの更新 Clickawayコンポーネントでラップする

--- src/components/molucules/WindowFrame.tsx
+++ src/components/molucules/WindowFrame.tsx
@@ -1,22 +1,26 @@
 import React, { ReactNode, VFC } from 'react'
 import MyImg from '../atoms/MyImg'
 import styles from '@/styles/components/molucules/WindowFrame.module.scss'
-import { style } from '@mui/system'
+import { Box, ClickAwayListener } from '@mui/material'
 
 interface PROPS {
   className?: string,
-  children: ReactNode
+  children: ReactNode,
+  clickaway: () => void
 }
 
 const WindowFrame:VFC<PROPS> = ({
   className,
-  children
+  children,
+  clickaway
 }) => (
-  <div className={`${ className } ${ styles['window-frame'] }`}>
-    <MyImg src='/img/others/upper-frame.svg' className={ styles.frame }/>
-    { children }
-    <MyImg src='/img/others/bottom-frame.svg' className={ styles.frame }/>
-  </div>
+  <ClickAwayListener onClickAway={ clickaway }>
+    <Box className={ className }>
+      <MyImg src='/img/others/upper-frame.svg' className={ styles.frame }/>
+      { children }
+      <MyImg src='/img/others/bottom-frame.svg' className={ styles.frame }/>
+    </Box>
+  </ClickAwayListener>
 )
 
 export default WindowFrame
