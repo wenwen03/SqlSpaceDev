30c52792e243394f1c75c1aa333bfade3a47a3aa
furuta03 <hibiki19961208@gmail.com>
2021-11-13 17:20:46 +0900

    [update]TextAreaコンポーネントの更新 onChangeコールバック関数をporpsで受け取る

--- src/components/atoms/Textarea.tsx
+++ src/components/atoms/Textarea.tsx
@@ -1,20 +1,23 @@
 import TextField from '@mui/material/TextField';
-import React, { VFC } from 'react'
+import React, { ChangeEvent, VFC } from 'react'
 
 interface PROPS {
   className?: string,
   textareaClass?: string,
   label?: string,
-  rows?: number
+  rows?: number,
+  onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void
 }
 
 const Textarea: VFC<PROPS> = ({
+  onChange,
   className,
   textareaClass,
   label,
   rows = 1
 }) => (
   <TextField
+    onChange={ onChange }
     className={ className }
     label={ label }
     type='text'
