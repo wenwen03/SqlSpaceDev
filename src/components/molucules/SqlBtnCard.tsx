0e46b32d9b8ac6a3510587dfd8e03b4d541aee31
furuta03 <hibiki19961208@gmail.com>
2021-11-13 14:13:27 +0900

    [add]SqlBtnCardコンポーネントの実装

---
+++ src/components/molucules/SqlBtnCard.tsx
@@ -0,0 +1,43 @@
+import React, { VFC } from 'react'
+import Button from '../atoms/Button'
+import Card from '../atoms/Card'
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import styles from '@/styles/components/molucules/SqlBtnCard.module.scss'
+
+interface PROPS {
+  className?: string
+}
+
+const SqlBtnCard: VFC<PROPS> = ({
+  className
+}) => {
+  const hoge = () => console.log('hoge')
+  return (
+    <Card className={`${styles['sql-btn-card']} ${ className }` }>
+      <div className={ styles['secondary-btns'] }>
+        <Button
+          onClick={ hoge }
+          className={ styles['secondary-btn'] }
+        >
+          hint
+        </Button>
+        <Button
+          onClick={ hoge }
+          className={ styles['secondary-btn'] }
+        >
+          log
+        </Button>
+      </div>
+      <Button 
+        onClick={ hoge }
+        color='secondary' 
+        startIcon={ <DirectionsRunIcon/> }
+        className={ styles['run-btn'] }
+      >
+        run
+      </Button>
+    </Card>
+  )
+}
+
+export default SqlBtnCard
