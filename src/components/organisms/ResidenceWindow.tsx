ffbc1e829452a25db3a46a145cc199946807fb61
furuta03 <hibiki19961208@gmail.com>
2021-11-13 17:28:08 +0900

    [update]ResidenceWindowコンポーネントの更新 residence情報を親コンポーネントから受ける

--- src/components/organisms/ResidenceWindow.tsx
+++ src/components/organisms/ResidenceWindow.tsx
@@ -3,26 +3,20 @@ import TableWindow from '../molucules/TableWindow'
 import TitleWindow from '../molucules/TitleWindow'
 import WindowFrame from '../molucules/WindowFrame'
 import styles from '@/styles/components/organisms/ResidenceWindow.module.scss'
-
-const ROWS = [
-  { id: 101, name: 'furuta', 'moved_at': '2021/11/11'},
-  { id: 101, name: 'furuta', 'moved_at': '2021/11/11'},
-];
-
-const COLUMNS = [
-  'id', 'name', 'moved_at'
-]
+import ResidenceEntity from '@/types/ResidenceEntity'
 
 interface PROPS {
   clickaway: () => void,
   startSQL: () => void
-  endSQL: () => void
+  endSQL: () => void,
+  entity: ResidenceEntity
 }
 
 const ResidenceWindow: VFC<PROPS> = ({
   clickaway,
   startSQL,
-  endSQL
+  endSQL,
+  entity
 }) => {
 
   // テーブル表示切替
@@ -48,10 +42,10 @@ const ResidenceWindow: VFC<PROPS> = ({
             max={0} 
           /> :
           <TableWindow
-            title='yamada-haitsu'
+            title={ entity.name }
             onClick={ toTitle }
-            columns={ COLUMNS }
-            rows={ ROWS }
+            columns={ entity.columns }
+            rows={ entity.rows }
           />
         }
       </WindowFrame>
