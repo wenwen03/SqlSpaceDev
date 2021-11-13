c1fdc7d12b7c54c5013dbbb4b5fae2ebb5503175
furuta03 <hibiki19961208@gmail.com>
2021-11-13 17:21:37 +0900

    [update]Monitorコンポーネントの更新 入力されたSQLqueryを親コンポーネントへ送る機能追加

--- src/components/molucules/Monitor.tsx
+++ src/components/molucules/Monitor.tsx
@@ -1,18 +1,21 @@
-import React, { VFC } from 'react'
+import React, { ChangeEvent, VFC } from 'react'
 import Textarea from '@/components/atoms/Textarea'
 import styles from '@/styles/components/molucules/Monitor.module.scss'
 import SideFrame from '@/components/molucules/SideFrame'
 
 interface PROPS {
   className: string
+  emitQuery: (e: ChangeEvent<HTMLTextAreaElement>) => void
 }
 
 const Monitor: VFC<PROPS> = ({
-  className
+  className,
+  emitQuery
 }) => {
   return (
     <SideFrame className={ className } >
       <Textarea
+        onChange={ emitQuery }
         className={ styles.textarea }
         textareaClass={ styles.input } 
         rows={11} 
