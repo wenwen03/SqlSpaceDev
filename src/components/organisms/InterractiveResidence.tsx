f6efa89ee42dc34e2c090749793b6c81d5485efc
furuta03 <hibiki19961208@gmail.com>
2021-11-13 10:30:20 +0900

    [update]InteractiveResidenceコンポーネントの更新 start/endSQLコールバック関数をpropsで受け取る

--- src/components/organisms/InterractiveResidence.tsx
+++ src/components/organisms/InterractiveResidence.tsx
@@ -4,18 +4,28 @@ import ResidenceWindow from './ResidenceWindow'
 import styles from '@/styles/components/organisms/InteractiveResidence.module.scss'
 
 interface PROPS {
+  startSQL: () => void
+  endSQL: () => void
 }
 
-const InterractiveResidence: VFC<PROPS> = () => { 
+const InterractiveResidence: VFC<PROPS> = ({
+  startSQL,
+  endSQL
+}) => { 
 
   const [ focusFlg, setFocusFlg ] = useState(false);
-  const focus = () => setFocusFlg(pre => !pre)
-  const clickaway = () => setFocusFlg(false)
+  const focus = () => {
+    setFocusFlg(true)
+  }
+  const clickaway = () => {
+    // setFocusFlg(false)
+    // endSQL()
+  }
 
   return (
     <div className={ styles['interactive-residence'] }>
       <Residence focusFunc={ focus }/>
-      { focusFlg && <ResidenceWindow clickaway={ clickaway } /> }
+      { focusFlg && <ResidenceWindow startSQL={ startSQL } endSQL={ endSQL } clickaway={ clickaway } /> }
     </div>
   )
  }
