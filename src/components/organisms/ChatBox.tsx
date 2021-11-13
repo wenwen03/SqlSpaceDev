9859a2c112e2d492e09086e5309aaa30d62e4e51
furuta03 <hibiki19961208@gmail.com>
2021-11-13 18:24:49 +0900

    [update]ChatBoxコンポーネントの更新 クリックしたら会話を先に進める機能追加

--- src/components/organisms/ChatBox.tsx
+++ src/components/organisms/ChatBox.tsx
@@ -1,28 +1,39 @@
-import React, { VFC } from 'react'
+import React, { useState, VFC } from 'react'
 import Bubble from '../molucules/Bubble'
 import styles from '@/styles/components/organisms/ChatBox.module.scss'
+import Chat from '@/types/Chat'
 
 interface PROPS {
-  sqlFlg: boolean
+  sqlFlg: boolean,
+  chatList: Array<Chat>
 }
 
+/** 画面に表示するメッセージ数 */
+const DISPLAY_COUNT = 3
+
 const ChatBox: VFC<PROPS> = ({
-  sqlFlg
+  sqlFlg,
+  chatList
 }) => {
+  const [step, setStep] = useState(1)
+  const startStep = step - DISPLAY_COUNT < 0 ? 0 : step - DISPLAY_COUNT
+  const nextStep = function(): void {
+    step === chatList.length ?
+      void(0) : setStep(pre => pre += 1)
+  }
   return (
-    <div className={ `${sqlFlg ? styles['sql-mode'] : '' }  ${ styles['chat-box'] }` }>
-      <Bubble
-        className={ styles.bubble }
-        speaker='human' 
-        comment='いよいよ初めてのお仕事ですね！
-          僕たちがこの星に最初の住人となるのです！
-          まずは僕たちの部屋を建設しましょう。'
-      />
-      <Bubble
-        className={ styles.bubble }
-        speaker='dog' 
-        comment='じゃあ、ボタンを押して部屋作成を始めてみましょう。'
-      />
+    <div
+      className={ `${sqlFlg ? styles['sql-mode'] : '' }  ${ styles['chat-box'] }` }
+      onClick={ nextStep }
+    >
+      { chatList.slice(startStep, step).map(( bubble, index ) =>
+        <Bubble
+          key={ index }
+          className={ styles.bubble }
+          speaker={ bubble.speaker }
+          comment={ bubble.comment }
+        />
+      )}
     </div>
   )
 }
