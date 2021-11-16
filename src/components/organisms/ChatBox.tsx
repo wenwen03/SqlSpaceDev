b66aa11c1a057c0adda790f66d37a8505b4a618b
furuta03 <hibiki19961208@gmail.com>
2021-11-16 09:52:43 +0900

    [update]ChatBoxコンポーネントの更新 コメントの進捗管理stateを外部から取得するようにする

--- src/components/organisms/ChatBox.tsx
+++ src/components/organisms/ChatBox.tsx
@@ -7,30 +7,22 @@ import FadeInOut from '../animations/FadeInOut'
 
 interface PROPS {
   sqlFlg: boolean,
-  chatList: Array<Chat>
+  chatList: Array<Chat>,
+  step: number
 }
-
-/** 画面に表示するメッセージ数 */
-const DISPLAY_COUNT = 3
 
 const ChatBox: VFC<PROPS> = ({
   sqlFlg,
-  chatList
+  chatList,
+  step
 }) => {
-  const [step, setStep] = useState(1)
-  const startStep = 0
-  const nextStep = function(): void {
-    step === chatList.length ?
-      void(0) : setStep(pre => pre += 1)
-  }
 
   return (
     <TransitionGroup
       component='div'
       className={ `${sqlFlg ? styles['sql-mode'] : '' }  ${ styles['chat-box'] }` }
-      onClick={ nextStep }
     >
-      { chatList.slice(startStep, step).map(( bubble, index ) =>
+      { chatList.slice(0, step).map(( bubble, index ) =>
         <FadeInOut key={ index }>
           <Bubble
             className={ styles.bubble }
