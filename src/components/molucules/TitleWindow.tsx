bd6cdf726e12119dc7119193a86862a138ec390e
furuta03 <hibiki19961208@gmail.com>
2021-11-16 12:35:33 +0900

    [update]TitleWindowコンポーネントの更新 TitleWindowのクリック時にdispatchするように変更

--- src/components/molucules/TitleWindow.tsx
+++ src/components/molucules/TitleWindow.tsx
@@ -5,6 +5,9 @@ import P from '../atoms/P'
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import styles from '@/styles/components/molucules/TitleWindow.module.scss'
 import H3 from '../atoms/H3'
+import { useMissionState } from '@/redux/missions/selectors';
+import { useDispatch } from 'react-redux';
+import missionSlice from '@/redux/missions/slice';
 
 interface PROPS {
   current: number,
@@ -17,12 +20,20 @@ const TitleWindow: VFC<PROPS> = ({
   onClick
 }) => {
 
+  const mission = useMissionState().mission
+  const dispatch = useDispatch()
+  const clickBtn = () => {
+    dispatch(missionSlice.actions.nextStep('TitleWindowBtn'))
+    onClick()
+  }
+
   return (
     <Card className={ styles['title-window']}>
       <H3>Residence</H3>
       <P>部屋数 { current } / { max }</P>
       <Button 
-        onClick={ onClick } 
+        isEmphasize={ mission.isEmphasizeBtn }
+        onClick={ clickBtn } 
         startIcon={ <ControlPointIcon/> }
       >
         create room
