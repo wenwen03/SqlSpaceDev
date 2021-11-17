2145cc01832e155bba82e19b75dfa4d734601083
furuta03 <hibiki19961208@gmail.com>
2021-11-17 12:00:52 +0900

    [update]RewardCardコンポーネントの更新 名称変更をヒントを表示するように変更

---
+++ src/components/molucules/HintCard.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode, VFC } from 'react'
+import Card from '@/components/atoms/Card'
+import H6 from '@/components/atoms/H6'
+import P from '../atoms/P'
+import styles from '@/styles/components/molucules/HintCard.module.scss'
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import { useMissionState } from '@/redux/missions/selectors'
+
+interface PROPS {
+  className?: string
+}
+
+const RewardCard: VFC<PROPS> = ({
+  className
+}) => {
+
+  const state = useMissionState().mission
+
+  return (
+  <Card className={ className }>
+    <div className={ styles.title }>
+        <LightbulbIcon/>
+        <H6 className={ styles.title }>ヒント</H6>
+    </div>
+    <P>{ state.goal }</P>
+  </Card>
+)}
+
+export default RewardCard
