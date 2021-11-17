f059e7136734a2979098200224387208055b34c8
furuta03 <hibiki19961208@gmail.com>
2021-11-17 17:09:04 +0900

    [update]missionページの更新 RunResultの結果文を追加

--- src/pages/mission.tsx
+++ src/pages/mission.tsx
@@ -42,7 +42,7 @@ const mission: VFC = () => {
     let record: ResidenceRow;
     switch(residenceList[0].rows.length) {
       case 0: record = { 'room_no': 101, name: 'dog', 'moved_at': today }; break;
-      case 1: record = { 'room_no': 102, name: 'test_user', 'moved_at': today }; break;
+      case 1: record = { 'room_no': 102, name: 'test-user', 'moved_at': today }; break;
       case 2: record = { 'room_no': 103, name: 'zako', 'moved_at': today }; break;
     }
 
@@ -72,7 +72,7 @@ const mission: VFC = () => {
       { !sqlFlg && <BottomMenu /> }
       { sqlFlg && <SQLRunner sqlAPI={ sqlAPI } /> }
       <CompleteModal/>
-      <RunResultWindow isOpen={showResultModal} onClose={closeResultModal} result=''/>
+      <RunResultWindow isOpen={showResultModal} onClose={closeResultModal} result='Query OK, 1 row affected (0.00 sec)'/>
       <ChatBox 
         step={ state.step }
         sqlFlg={ sqlFlg }
