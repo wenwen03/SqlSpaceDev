37012a95179f03fdc56efd52743bfbddb438fc33
furuta03 <hibiki19961208@gmail.com>
2021-11-17 20:02:41 +0900

    [update]SqlRunnerコンポーネントの更新 ResidenceListの更新メソッドをReduxからdispatchでコールする

--- src/components/organisms/SQLRunner.tsx
+++ src/components/organisms/SQLRunner.tsx
@@ -3,23 +3,43 @@ import styles from '@/styles/components/organisms/SQLRunner.module.scss'
 import Monitor from '../molucules/Monitor'
 import RewardCard from '../molucules/HintCard'
 import SqlBtnCard from '../molucules/SqlBtnCard'
+import { useMissionState } from '@/redux/missions/selectors'
+import { useDispatch } from 'react-redux'
+import ResidenceRow from '@/types/ResidenceRow'
+import missionSlice from '@/redux/missions/slice'
 
 interface PROPS {
-  sqlAPI: (query: string) => void
 }
 
-const SQLRunner: VFC<PROPS> = ({
-  sqlAPI 
-}) => {
+const SQLRunner: VFC<PROPS> = () => {
+
+  const state = useMissionState().mission
+  const dispatch = useDispatch()
+
+  const sqlAPI = function(): void {
+
+    const today = new Date().toLocaleDateString()
+
+    let record: ResidenceRow;
+    switch( state.residenceList[0].rows.length) {
+      case 0: record = { 'room_no': 101, name: 'dog', 'moved_at': today }; break;
+      case 1: record = { 'room_no': 102, name: 'test-user', 'moved_at': today }; break;
+      case 2: record = { 'room_no': 103, name: 'zako', 'moved_at': today }; break;
+    }
+
+    dispatch(missionSlice.actions.pushResidenceList(record))
+    dispatch(missionSlice.actions.updateShowResultModal(true))
+  }
+
+
   const [ query, setQuery ] = useState('')
   const emitQuery = (e: ChangeEvent<HTMLTextAreaElement>)=> setQuery(e.target.value)
-  const run = () => sqlAPI(query)
 
-  return (
+  return state.isSqlMode && (
     <div className={ styles['sql-runner'] }>
       <Monitor className={ styles.monitor } emitQuery={ emitQuery }/>
       <RewardCard className={ styles['reward-card'] } />
-      <SqlBtnCard className={ styles['sql-btn-card'] }  run={ run }/>
+      <SqlBtnCard className={ styles['sql-btn-card'] }  run={ sqlAPI }/>
     </div>
   )
 }
