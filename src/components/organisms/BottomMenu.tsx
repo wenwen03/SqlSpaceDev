491f94eac4b4fc1d2f7a878537a557c30a268a83
furuta03 <hibiki19961208@gmail.com>
2021-11-17 19:56:59 +0900

    [update]BottomMenuコンポーネントの更新 表示フラグをreduxから取得する

--- src/components/organisms/BottomMenu.tsx
+++ src/components/organisms/BottomMenu.tsx
@@ -2,9 +2,13 @@ import React, { VFC } from 'react'
 import styles from '@/styles/components/organisms/BottomMenu.module.scss'
 import ResourceBar from './ResourceBar'
 import CreateBtn from '../molucules/CreateBtn'
+import { useMissionState } from '@/redux/missions/selectors'
 
 const BottomMenu: VFC = () => {
-  return (
+
+  const state = useMissionState().mission
+
+  return  !state.isSqlMode && (
     <div className={ styles['bottom-menu'] }>
       <ResourceBar />
       <CreateBtn />
