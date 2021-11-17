07b901e940b66025980e6ec87c39fad630f655b9
furuta03 <hibiki19961208@gmail.com>
2021-11-17 20:01:17 +0900

    [fix]SkillLineコンポーネントの軽微な修正 viewBox属性の値が不正なためエラーが出ていたが、そもそも不要なので削除

--- src/components/organisms/SkillLines.tsx
+++ src/components/organisms/SkillLines.tsx
@@ -64,7 +64,7 @@ const SkillLines: VFC = () => {
 
   return (
     <>
-      <svg width="100%" height="100%" viewBox="0 0 100% 100%" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="100%" height="100%" fill="none" xmlns="http://www.w3.org/2000/svg">
         {matrix.map(( cordinate, index ) => (
           <line
             x1={lineX(cordinate.x1)}
