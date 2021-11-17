413c44b787136b4e0dc74168f7b893b635f18c53
furuta03 <hibiki19961208@gmail.com>
2021-11-17 19:59:49 +0900

    [update]sliceファイルの更新 Sqlモードフラグ/モーダル表示フラグ/ResidenceListなどをreduxで扱うように更新

--- src/redux/missions/slice.ts
+++ src/redux/missions/slice.ts
@@ -2,6 +2,8 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import Missin01 from '@/scenarios/Mission01'
 import Chat from '@/types/Chat';
 import MissionFunc01 from '@/scenarios/MissionFunc01';
+import ResidenceEntity from '@/types/ResidenceEntity';
+import ResidenceRow from '@/types/ResidenceRow';
 
 export interface MissionState {
   isEmphasize: Object,
@@ -9,7 +11,10 @@ export interface MissionState {
   step: number,
   goal: string,
   scenario: Array<Chat>
-  showCompleteModal: boolean
+  showCompleteModal: boolean,
+  showResultModal: boolean,
+  isSqlMode: boolean,
+  residenceList: Array<ResidenceEntity>
 };
 
 export const initialState: MissionState = {
@@ -18,7 +23,16 @@ export const initialState: MissionState = {
   step: 1,
   goal: '画面をクリックするとストリーが進みます',
   scenario: Missin01,
-  showCompleteModal: false
+  showCompleteModal: false,
+  showResultModal: false,
+  isSqlMode: false,
+  residenceList: [
+    {
+      name: 'house01',
+      columns: ['room_no', 'name', 'moved_at'],
+      rows:  []
+    }
+  ]
 };
 
 const missionSlice = createSlice({
@@ -36,7 +50,18 @@ const missionSlice = createSlice({
     nextStep: (state, action: PayloadAction<string>) => {
       if(MissionFunc01[state.step].condition !== action.payload) return
       MissionFunc01[state.step].stepUpFunction(state)
-    }
+    },
+    updateSqlMode: (state, action: PayloadAction<boolean>) => ({
+      ...state,
+      isSqlMode: action.payload
+    }),
+    updateShowResultModal: (state, action: PayloadAction<boolean>) => ({
+      ...state,
+      showResultModal: action.payload
+    }),
+    pushResidenceList: (state, action: PayloadAction<ResidenceRow>) => {
+      state.residenceList[0].rows.push(action.payload)
+    },
   },
 });
 
