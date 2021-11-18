27cc25b717cd2ff0c3015c55a0c74abcb9a77c86
furuta03 <hibiki19961208@gmail.com>
2021-11-18 16:53:00 +0900

    [update]Selectページの変更 モーダル表示フラグ/Sqlモードフラグをページファイルで管理する

--- src/pages/mission/select.tsx
+++ src/pages/mission/select.tsx
@@ -1,6 +1,6 @@
 import BottomMenu from '@/components/organisms/BottomMenu'
 import Mission from '@/components/templates/Mission'
-import React, { VFC } from 'react'
+import React, { useState, VFC } from 'react'
 import styles from '@/styles/pages/insert.module.scss'
 import ResidenceList from '@/components/organisms/ResidenceList'
 import ChatBox from '@/components/organisms/ChatBox'
@@ -10,23 +10,33 @@ import RunResultWindow from '@/components/organisms/RunResultWindow'
 import { useDispatch } from 'react-redux'
 import missionSlice from '@/redux/missions/slice'
 import SelectScenario from '@/scenarios/SelectScenario'
+import SpaceWoodList from '@/components/organisms/SpaceWoodList'
 
 const Select: VFC = () => {
 
   const dispatch = useDispatch()
   dispatch(missionSlice.actions.setInitialState(SelectScenario))
 
+  const [isSqlMode, setIsSqlMode] = useState(false)
+  const [showResultModal, setShowResultModal] = useState(false)
+
   return (
-    <Mission 
+    <Mission
       title='Select文の基本 | SQL Space Dev'
       wholeClass={ styles.layout }
     >
-      <ResidenceList />
-      <ChatBox />
-      <SQLRunner />
       <CompleteModal/>
-      <RunResultWindow result='Query OK, 1 row affected (0.00 sec)'/>
-      <BottomMenu />
+      <ResidenceList setIsSqlMode={ setIsSqlMode }/>
+      <SpaceWoodList setIsSqlMode={ setIsSqlMode }/>
+      { !isSqlMode && <BottomMenu /> }
+      { isSqlMode && <SQLRunner setShowResultModal={ setShowResultModal}/> }
+      {
+        showResultModal && <RunResultWindow 
+        isOpen={ showResultModal } 
+        setShowResultModal={ setShowResultModal } 
+        result='Query OK, 1 row affected (0.00 sec)'/> 
+      }
+      <ChatBox />
     </Mission>
   )
 }
