a5338ed4217e2aed23e27172d48364cb5ee59be4
furuta03 <hibiki19961208@gmail.com>
2021-11-18 18:16:25 +0900

    [fix]BottomMenuコンポーネントの修正 isSqlModeフラグの削除

--- src/components/organisms/BottomMenu.tsx
+++ src/components/organisms/BottomMenu.tsx
@@ -2,18 +2,12 @@ import React, { VFC } from 'react'
 import styles from '@/styles/components/organisms/BottomMenu.module.scss'
 import ResourceBar from './ResourceBar'
 import CreateBtn from '../molucules/CreateBtn'
-import { useMissionState } from '@/redux/missions/selectors'
 
-const BottomMenu: VFC = () => {
-
-  const state = useMissionState().mission
-
-  return  !state.isSqlMode && (
-    <div className={ styles['bottom-menu'] }>
-      <ResourceBar />
-      <CreateBtn />
-    </div>
-  )
-}
+const BottomMenu: VFC = () => (
+  <div className={ styles['bottom-menu'] }>
+    <ResourceBar />
+    <CreateBtn />
+  </div>
+)
 
 export default BottomMenu
