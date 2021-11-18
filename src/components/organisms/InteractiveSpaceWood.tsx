c701ce3a099302d770b5fab3d85d5523aa38b0d3
furuta03 <hibiki19961208@gmail.com>
2021-11-18 16:43:18 +0900

    [add]InteractiveSpaceWoodコンポーネントの実装

---
+++ src/components/organisms/InteractiveSpaceWood.tsx
@@ -0,0 +1,39 @@
+import ResidenceEntity from '@/types/ResidenceEntity';
+import React, { useState, VFC } from 'react'
+import SpaceWood from '../molucules/SpaceWood';
+import styles from '@/styles/components/organisms/InteractiveSpaceWood.module.scss'
+import ArchitectureWindow from './ArchitectWindow';
+
+interface PROPS {
+  entity: ResidenceEntity,
+  setIsSqlMode: (boolean) => void
+}
+
+const InteractiveSpaceWood: VFC<PROPS> = ({
+  entity,
+  setIsSqlMode
+}) => {
+
+  const [ focusFlg, setFocusFlg ] = useState(false);
+  const focus = () => setFocusFlg(true)
+
+  const clickaway = () => {
+    // setFocusFlg(false)
+    // endSQL()
+  }
+
+  return (
+    <div className={ styles['interactive-space-wood'] }>
+      <SpaceWood onFocus={ focus }/>
+      { 
+        focusFlg && <ArchitectureWindow
+          setIsSqlMode={ setIsSqlMode }
+          entity={ entity }
+          clickaway={ clickaway } 
+        />
+      }
+    </div>
+  )
+}
+
+export default InteractiveSpaceWood
