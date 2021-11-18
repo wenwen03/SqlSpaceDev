4fde12faeddb7369705fb369f5522517511633cc
furuta03 <hibiki19961208@gmail.com>
2021-11-18 16:28:13 +0900

    [update]SpaceWoodコンポーネントの更新 クリックでdispatchする関数の追加

--- src/components/molucules/SpaceWood.tsx
+++ src/components/molucules/SpaceWood.tsx
@@ -1,15 +1,33 @@
 import React, { VFC } from 'react'
 import MyImg from '@/components/atoms/MyImg'
 import styles from '@/styles/components/molucules/Architecture.module.scss'
+import { useDispatch } from 'react-redux'
+import missionSlice from '@/redux/missions/slice'
 
-const SpaceWood: VFC = () => (
-  <MyImg
-    src='/img/resources/space-wood.svg'
-    alt='Space Wood'
-    className={ styles.architecture }
-    width={ 200 }
-    height={ 200 }
-  />
-)
+interface PROPS {
+  onFocus: () => void
+}
+
+const SpaceWood: VFC<PROPS> = ({
+  onFocus
+}) => {
+
+  const dispatch = useDispatch()
+  const onClick = () => {
+    dispatch(missionSlice.actions.nextStep('spaceWood'))
+    onFocus()
+  }
+
+  return (
+    <MyImg
+      onClick={ onClick }
+      src='/img/resources/space-wood.svg'
+      alt='Space Wood'
+      className={ styles.architecture }
+      width={ 200 }
+      height={ 200 }
+    />
+  )
+}
 
 export default SpaceWood
