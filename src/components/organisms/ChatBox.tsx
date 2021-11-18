081161e6bb1242a32fcbcdc278644a6b196edbb7
furuta03 <hibiki19961208@gmail.com>
2021-11-18 14:47:52 +0900

    [update]ChatBoxコンポーネントの更新 シナリオをreduxから取得する

--- src/components/organisms/ChatBox.tsx
+++ src/components/organisms/ChatBox.tsx
@@ -5,24 +5,23 @@ import { TransitionGroup } from 'react-transition-group'
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import FadeInOut from '../animations/FadeInOut'
 import { useMissionState } from '@/redux/missions/selectors'
-import InsertScenario from '@/scenarios/InsertScenario';
-
 
 const ChatBox: VFC = () => {
 
   const state = useMissionState().mission
+  const scenario = state.scenario
 
   // 表示セリフ更新時に一番下までスクロール
   const bottomRef = useRef<HTMLDivElement>(null)
   useLayoutEffect(() => {
     bottomRef.current?.scrollIntoView()
-  }, [ InsertScenario.slice(0, state.step) ])
+  }, [ scenario.slice(0, state.step) ])
 
   return (
     <div className={ `${ state.isSqlMode ? styles['sql-mode'] : '' }  ${ styles['chat-box'] }` }>
       <div className={ styles.info }><PlayCircleFilledWhiteIcon/>画面上をクリックして話を進める</div>
       <TransitionGroup component='div' >
-        { InsertScenario.slice(0, state.step).map(( scenarioStep, index ) =>
+        { scenario.slice(0, state.step).map(( scenarioStep, index ) =>
           <FadeInOut key={ index }>
             <Bubble
               className={ styles.bubble }
