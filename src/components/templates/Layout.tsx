8f9895674a9d81c37be23d0a29aa1b281b26bdc8
furuta03 <hibiki19961208@gmail.com>
2021-11-20 15:37:43 +0900

    [update]Layoutテンプレートの更新 mainをクリックするとdispatchする処理の削除

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -2,8 +2,6 @@ import { ReactNode, VFC } from 'react'
 import styles from "@/styles/components/templates/Layout.module.scss"
 import Head from 'next/head'
 import Navbar from '../organisms/Navbar'
-import { useDispatch } from 'react-redux'
-import missionSlice from '@/redux/missions/slice'
 
 interface PROPS {
   children: ReactNode,
@@ -17,29 +15,22 @@ const Layout: VFC<PROPS> = ({
   title,
   wholeClass,
   mainClass
-}) => {
-
-  const dispatch = useDispatch()
-  const onClick = () => dispatch(missionSlice.actions.nextStep('main'))
-
-  return (
-    <div className={`${ wholeClass } ${ styles.layout }`}>
-      <Head>
-        <title>{ title }</title>
-      </Head>
-      <header className={ styles.header }>
-        <Navbar/>
-      </header>
-      <main 
-        className={`${ mainClass } ${ styles.main }`}
-        onClick={ onClick }
-      >
-        { children }
-      </main>
-      <footer>
-      </footer>
-    </div>
-  )
-}
+}) => (
+  <div className={`${ wholeClass } ${ styles.layout }`}>
+    <Head>
+      <title>{ title }</title>
+    </Head>
+    <header className={ styles.header }>
+      <Navbar/>
+    </header>
+    <main 
+      className={`${ mainClass } ${ styles.main }`}
+    >
+      { children }
+    </main>
+    <footer>
+    </footer>
+  </div>
+)
 
 export default Layout;
