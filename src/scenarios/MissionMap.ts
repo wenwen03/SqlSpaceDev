e18a61b00a4caf114ec99e1206538f8c9662ba03
furuta03 <hibiki19961208@gmail.com>
2021-11-20 16:03:49 +0900

    [update]MissionMapの更新 Missionの追加

--- src/scenarios/MissionMap.ts
+++ src/scenarios/MissionMap.ts
@@ -4,7 +4,7 @@ const missionMap: MissionMap = {
   Insert: {
     title: 'Inset文の基本を学ぼう',
     nextPath: '/mission/select' ,
-    description: 'この会社には研修がなく、教育制度が整っていないみたい。\nテキトーに応募したのがよくなかったね！\n\nこのミッションでは、SQLができることなどを実際にSQLを使用しながら\n学んでいきます。SQLマスターの第一歩を踏み出そう！',
+    description: 'このミッションにはInsert文を使って住居に部屋を増設することを学びます。\n基本コマンドの１つをマスターしましょう！',
     purposes: ['SQLとは何かを理解する', 'SQLでできることを理解する']
   },
   Select: {
@@ -12,7 +12,116 @@ const missionMap: MissionMap = {
     nextPath: '/mission/select' ,
     description: '都市開発をしようにも材料がないから建設できないみたい。Select文を駆使して必要な資源を探してこよう。',
     purposes: ['基本的なSelect文を扱えるようになる', 'Where句で条件指定ができる']
-  }
+  },
+  Update: {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Delete: {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Create: {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Where: {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Undefined Null': {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Bulk Command':  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Basic Function':  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Ambiguous Search':  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Range Search':  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Group:  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Arthmetic Operator':  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Index:  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Union Except Intersect':  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Pagenation:  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  'Aggregate Function':  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Join:  {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Case: {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+  Performance: {
+    title: 'Locked',
+    nextPath: '/' ,
+    description: 'not yet implemented',
+    purposes: []
+  },
+
 }
 
 export default missionMap
--- src/types/Mission.d.ts
+++ src/types/Mission.d.ts
@@ -7,5 +7,23 @@ export interface Mission {
 
 export default interface MissionMap {
   Insert: Mission,
-  Select: Mission
+  Select: Mission,
+  Update: Mission,
+  Delete: Mission,
+  Create: Mission,
+  Where: Mission,
+  'Undefined Null': Mission,
+  'Bulk Command': Mission,
+  'Basic Function': Mission,
+  'Ambiguous Search': Mission,
+  'Range Search': Mission,
+  Group: Mission,
+  'Arthmetic Operator': Mission,
+  Index: Mission,
+  'Union Except Intersect': Mission,
+  Pagenation: Mission,
+  'Aggregate Function': Mission,
+  Join: Mission,
+  Case: Mission,
+  Performance: Mission
 }
