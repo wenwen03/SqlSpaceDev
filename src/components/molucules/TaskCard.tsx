64a8aade52f82ef7ffb76039b6415ee011c8702a
furuta03 <hibiki19961208@gmail.com>
2021-11-20 11:11:43 +0900

    [update]TaskCardコンポーネントの更新 taskStepをreduxから取得する

--- src/components/molucules/TaskCard.tsx
+++ src/components/molucules/TaskCard.tsx
@@ -1,11 +1,11 @@
 import { useMissionState } from '@/redux/missions/selectors'
 import styles from '@/styles/components/molucules/TaskCard.module.scss'
 import animations from '@/styles/components/animations/Emphasize.module.scss';
-import React, { useState, VFC } from 'react'
+import React, { VFC } from 'react'
 import Card from '../atoms/Card'
 
+
 const TaskCard:VFC = () => {
-  const [taskStep, setTaskStep] = useState(0)
   const state = useMissionState().mission
   return (
     <Card 
@@ -14,7 +14,7 @@ const TaskCard:VFC = () => {
         ${ state.isEmphasize['taskCard'] ? animations.emphasize : '' }
       `}
     >
-      { state.tasks[taskStep] }
+      { state.tasks[state.taskStep] }
     </Card>
   )
 }
