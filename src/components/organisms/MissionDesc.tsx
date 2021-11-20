bb32efe7eefd27dd91f5b5c79d8db4ab3dec6ed9
furuta03 <hibiki19961208@gmail.com>
2021-11-20 19:28:50 +0900

    [update]MissionDescコンポーネントの更新 Gif画像の追加

--- src/components/organisms/MissionDesc.tsx
+++ src/components/organisms/MissionDesc.tsx
@@ -2,6 +2,8 @@ import React, { VFC } from 'react'
 import Bubble from '@/components/molucules/Bubble'
 import Image from 'next/image'
 import styles from '@/styles/components/organisms/MissionDesc.module.scss'
+import { useMissionState } from '@/redux/missions/selectors'
+import MissionMap from '@/scenarios/MissionMap'
 
 interface PROPS {
   className: string
@@ -9,21 +11,13 @@ interface PROPS {
 
 const MissionDesc: VFC<PROPS> = ({
   className
-}) => (
-  <div className={ `${ styles['mission-desc'] } ${ className }`}>
-    <Bubble
-      className={ styles['human-comment'] }
-      comment='君が新入社員の「」さんですね。今年は新卒で応募してきたのが君しか居なかったから早速現場で働いてもらいますよ！'
-      speaker='human'
-    />
-    <Bubble
-      className={ styles['dog-comment'] }
-      comment='幸運なことに君は、新しく開発する都市に移住しながら住み込みで働けるよ！準備できたら早速向かおう！'
-      speaker='dog'
-    />
-    <Image className={ styles.img } src='/img/character/human.png' width={200} height={200}/>
-    <Image className={ styles.img } src='/img/character/dog.png' width={100} height={100} />
-  </div>
-)
-
+}) => {
+  const state = useMissionState().mission
+  const imgPath = MissionMap[state.missionName].imgPath
+  return (
+    <div className={ `${ styles['mission-desc'] } ${ className }`}>
+      <Image className={ styles.img } src={ imgPath } width={400} height={300}/>
+    </div>
+  )
+}
 export default MissionDesc
